449f911a5bd297b68700977ca716e92346ec6172
blonded <iguessitsfuckme@proton.me>
2024-07-15 00:11:47 +0100

    Add functionality to print out vote details in chat (type of vote, who started it, who voted for it)

---
+++ src/radioracers/rr_cvar.h
@@ -0,0 +1,31 @@
+// RadioRacers
+//-----------------------------------------------------------------------------
+// Copyright (C) 2024 by $HOME
+//
+// This program is free software distributed under the
+// terms of the GNU General Public License, version 2.
+// See the 'LICENSE' file for more details.
+//-----------------------------------------------------------------------------
+/// \file radioracers/rr_cvar.h
+/// \brief RadioRacers CVARs
+
+// Separating custom functionality into new header and source code files is so much cleaner.    
+
+#ifndef __RR_CVAR__
+#define __RR_CVAR__
+
+// consvar_t
+#include "../command.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Player (Clientside)
+extern consvar_t cv_votesnitch;     // Vote Snitch
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif
