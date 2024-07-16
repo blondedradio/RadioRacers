06dd4f9e193d68d1eb8ad66a7fefef5b320c2487
blonded <iguessitsfuckme@proton.me>
2024-07-16 21:56:00 +0100

    Add new CVAR for extending controller rumble conditions in new rr_controller library and source file

--- src/k_kart.c
+++ src/k_kart.c
@@ -90,6 +90,11 @@ void K_PopBubbleShield(player_t *player)
 	player->bubbledrag = false;
 }
 
+// RadioRacers: Hacky ways of checking for events the exact FRAME that they happen
+boolean localPlayerJustRingBoosted = false; 	// The second your Ring Boost (TM) timer starts
+boolean localPlayerJustBootyBounced = false; 	// The second you start a fastfall bounce
+boolean localPlayerJustWavedashed = false;		// The few seconds or so your wavedash starts
+
 boolean K_ThunderDome(void)
 {
 	if (K_CanChangeRules(true))
@@ -13211,6 +13216,12 @@ static void K_KartDrift(player_t *player, boolean onground)
 						)
 					);
 
+					// RadioRacers: .. right around here.
+					if (P_IsMachineLocalPlayer(player) && !localPlayerJustWavedashed)
+					{
+						localPlayerJustWavedashed = true;
+					}
+
 					K_SpawnDriftBoostExplosion(player, 0);
 				}
 				S_StopSoundByID(player->mo, sfx_waved1);
@@ -14252,6 +14263,11 @@ boolean K_FastFallBounce(player_t *player)
 		else
 		{
 			S_StartSound(player->mo, sfx_ffbonc);
+			// RadioRacers: .. right around here.
+			if (P_IsMachineLocalPlayer(player) && !localPlayerJustBootyBounced)
+			{
+				localPlayerJustBootyBounced = true;
+			}
 		}
 
 		if (player->mo->eflags & MFE_UNDERWATER)
@@ -16346,6 +16362,12 @@ void K_MoveKartPlayer(player_t *player, boolean onground)
 			}
 		}
 
+	}
+
+	// RadioRacers: Really gross.
+	if (P_IsMachineLocalPlayer(player) && localPlayerJustWavedashed)
+	{
+		localPlayerJustWavedashed = false;
 	}
 
 	K_KartDrift(player, onground);
---
+++ src/radioracers/gameplay/rr_controllerrumble.c
@@ -0,0 +1,190 @@
+// RadioRacers
+//-----------------------------------------------------------------------------
+// Copyright (C) 2024 by $HOME
+//
+// This program is free software distributed under the
+// terms of the GNU General Public License, version 2.
+// See the 'LICENSE' file for more details.
+//-----------------------------------------------------------------------------
+/// \file radioracers/gameplay/rr_controllerrumble.c
+/// \brief Check for more gameplay conditions to vibrate the player's controller too (e.g. rings)
+
+#include "../rr_cvar.h"
+#include "../rr_controller.h"
+#include "../../doomtype.h"
+#include "../../p_local.h"
+#include "../../p_mobj.h"
+#include "../../d_player.h"
+#include "../../k_kart.h"
+
+boolean RR_ShouldUseMoreRumbleEvents(void)
+{
+    return (cv_morerumbleevents.value);
+}
+
+rumbleevent_e RR_GetRumbleEvent(const player_t* player)
+{
+    if (RR_RumbleJustBumpedWall(player) || RR_RumbleJustFastFallBounced(player))
+    {
+        return EVT_WALL;
+    }
+    else if (RR_RumbleJustDrifted(player))
+    {
+        return EVT_DRIFT;
+    }
+    else if (RR_RumbleIsSpindashingGently(player))
+    {
+        return EVT_SPINDASH_GENTLE;
+    }
+    else if (RR_RumbleIsSpindashingViolently(player))
+    {
+        return EVT_SPINDASH_VIOLENT;
+    }
+    else if (RR_RumbleOverchargingTailWhip(player))
+    {
+        return EVT_WHIP_OVERCHARGING;
+    }
+    else if (RR_RumbleChargingTailWhip(player))
+    {
+        return EVT_WHIP_CHARGING;
+    }
+    else if (RR_RumbleJustWaveDashed(player))
+    {
+        return EVT_WAVEDASHED;
+    }
+    else if (RR_RumbleRingCollected(player) || RR_RumbleRingConsumed(player))
+    {
+        return EVT_RING;
+    }
+
+    return 0;
+}
+
+uint16_t RR_GetRumbleStrength(const rumbleevent_e event)
+{
+    switch (event)
+    {
+        case EVT_WALL:
+            return (65536 / 2);
+        case EVT_WHIP_CHARGING:
+        case EVT_SPINDASH_GENTLE:
+            return (65536 / 25);
+        case EVT_WHIP_OVERCHARGING:
+        case EVT_SPINDASH_VIOLENT:
+            return (65536 / 10);
+        case EVT_WAVEDASHED:
+            return (65536 / 8);
+        case EVT_DRIFT:
+            return (65536 / 100);
+        case EVT_RING:
+            return (65536 / 160); // Way more subtle than stairjank rumble, cos rings can stack
+        default:
+            return 0xFFFF;
+    }
+}
+
+boolean RR_RumbleJustBumpedWall(const player_t *player)
+{
+    return (player->mo->eflags & MFE_JUSTBOUNCEDWALL);
+}
+
+boolean RR_RumbleIsSpindashingGently(const player_t *player)
+{
+    return (
+            P_IsObjectOnGround(player->mo) &&
+            player-> spindash && 
+            player-> spindash < K_GetSpindashChargeTime(player) 
+    );
+}
+
+boolean RR_RumbleIsSpindashingViolently(const player_t *player)
+{
+    return (
+            P_IsObjectOnGround(player->mo) &&
+            player-> spindash && 
+            player-> spindash > K_GetSpindashChargeTime(player) 
+    );
+}
+
+boolean RR_RumbleRingCollected(const player_t *player)
+{
+    return (
+        player->mo && 
+        player->rings < 20 &&
+        player->pickuprings > 0 && player->pickuprings > player->pickuprings-1 && player->pickuprings < player->pickuprings+1
+    );
+}
+
+boolean RR_RumbleChargingTailWhip(const player_t *player)
+{
+    return (
+        (player->mo) &&
+        player->instaWhipCharge != 0 &&
+        player->instaWhipCharge < INSTAWHIP_CHARGETIME
+    );
+}
+
+boolean RR_RumbleOverchargingTailWhip(const player_t *player)
+{
+    return (
+        (player->mo) &&
+        player->instaWhipCharge >= INSTAWHIP_CHARGETIME
+    );
+}
+
+boolean RR_RumbleJustDrifted(const player_t *player)
+{
+    // Charlie, Charlie! Have I told you how much I LOVE repeated code?!
+    const INT32 dsone = K_GetKartDriftSparkValueForStage(player, 1);
+    const INT32 dstwo = K_GetKartDriftSparkValueForStage(player, 2);
+    const INT32 dsthree = K_GetKartDriftSparkValueForStage(player, 3);
+    const INT32 dsfour = K_GetKartDriftSparkValueForStage(player, 4);
+
+    const boolean yellowSparkDrifts = (player->driftcharge >= (dsone - (32*3)) && player-> driftcharge < dsone);
+    const boolean redSparkDrifts = (player->driftcharge >= (dstwo - (32*3)) && player->driftcharge < dstwo);
+    const boolean blueSparkDrifts = (player->driftcharge >= (dsthree - (32*3)) && player->driftcharge < dsthree);
+    const boolean rainbowSparkDrifts = (player->driftcharge >= (dsfour - (32*3)) && player->driftcharge < dsfour);
+    
+    return (
+        player->mo &&
+        player->drift &&
+        (yellowSparkDrifts || redSparkDrifts || blueSparkDrifts || rainbowSparkDrifts)
+    );
+}
+/**
+ * These checks are kind of hacky. 
+ * But I want the rumble to happen the FRAME that the ringboost timer has started.
+ * That way, the player gets feedback immediately and knows when to start adjusting lines
+ * and the sort.
+ */
+boolean RR_RumbleRingConsumed(const player_t *player)
+{
+    const boolean ringJustConsumed = (
+        player->mo &&
+        localPlayerJustRingBoosted
+    );
+
+    if (ringJustConsumed) 
+        localPlayerJustRingBoosted = false;
+
+    return ringJustConsumed;
+}
+
+boolean RR_RumbleJustFastFallBounced(const player_t *player)
+{
+    return (
+        (player-> mo) &&
+        player->fastfall !=0 &&
+        P_IsObjectOnGround(player->mo) && 
+        localPlayerJustBootyBounced
+    );
+}
+
+boolean RR_RumbleJustWaveDashed(const player_t *player)
+{
+    return (
+        (player->mo) &&
+        P_IsObjectOnGround(player->mo) &&
+        localPlayerJustWavedashed
+    );
+}
