f5c5d03b9d21fedde395bb0abf4ef5e011623269
blonded <iguessitsfuckme@proton.me>
2025-04-30 20:48:34 +0100

    Add item timers accessibility feature
    Draw item timers in the middle-bottom screen of the current player (excl. splitscreen)

--- src/k_hud.cpp
+++ src/k_hud.cpp
@@ -8605,7 +8605,7 @@ void K_drawKartHUD(void)
 			tic_t realtime = stplyr->realtime;
 
 			// RADIO: Little thing
-			if (leveltime < starttime)
+			if (leveltime < starttime && gametype == GT_RACE)
 			{
 				realtime = starttime - leveltime;
 			}
@@ -9040,7 +9040,11 @@ void K_drawKartHUD(void)
 		K_drawMiniPing();
 	}
 
-	K_drawKartPowerUps();
+	if (cv_gingeritemtimers.value) {
+		RR_DrawItemTimers();
+	} else {
+		K_drawKartPowerUps();
+	}
 
 	if (K_DirectorIsAvailable(viewnum) == true && LUA_HudEnabled(hud_textspectator))
 	{
