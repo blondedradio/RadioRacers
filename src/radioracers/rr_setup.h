6e06e62b0208a6b7964b02d0f918c02382502220
blonded <iguessitsfuckme@proton.me>
2025-05-29 22:57:47 +0100

    Fix hide countdown cvar + add missing tripwire textures

--- src/radioracers/rr_setup.h
+++ src/radioracers/rr_setup.h
@@ -29,11 +29,19 @@ extern boolean radioracers_usealternatetripwire;
 
 // Base texture IDs
 extern INT32 RADIO_BADWIRE_TEX_ID;
+extern INT32 RADIO_BADWIRE_4X_TEX_ID;
+extern INT32 RADIO_BADWIRE_VERTICAL_TEX_ID;
 extern INT32 RADIO_GOODWIRE_TEX_ID;
+extern INT32 RADIO_GOODWIRE_4X_TEX_ID;
+extern INT32 RADIO_GOODWIRE_VERTICAL_TEX_ID;
 
 // Texture name
 extern const char* RADIO_BADWIRE_TEX_NAME;
+extern const char* RADIO_BADWIRE_4X_TEX_NAME;
+extern const char* RADIO_BADWIRE_VERTICAL_TEX_NAME;
 extern const char* RADIO_GOODWIRE_TEX_NAME;
+extern const char* RADIO_GOODWIRE_4X_TEX_NAME;
+extern const char* RADIO_GOODWIRE_VERTICAL_TEX_NAME;
 
 extern patch_t *end_key[2];
 extern sfxenum_t radio_ding_sound;
