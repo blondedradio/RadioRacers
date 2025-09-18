ea63b9a555958b0bf2de433f66d40fba827868ee
blonded <iguessitsfuckme@proton.me>
2025-09-18 15:22:29 +0100

    Add highreshudscale cvar

--- src/k_hud.cpp
+++ src/k_hud.cpp
@@ -2402,8 +2402,10 @@ static void K_drawBackupItem(void)
 	boolean drawingOnPlayer = (cv_rouletteonplayer.value == 1 && r_splitscreen == 0);
 	const fixed_t baseHudScaleFixed = (drawingOnPlayer) ? RR_getItemBoxHudScale() : FRACUNIT;
 	const float_t baseHudScaleFloat = RR_getItemBoxHudScaleFloat();
+	INT32 backupitemflags = transflag|V_SLIDEIN;
+	
 	if (drawingOnPlayer) {
-		transflag = V_20TRANS;
+		backupitemflags = V_20TRANS;
 		fx = itemRouletteX + ((int)(30 * baseHudScaleFloat));
 		fy = itemRouletteY - ((int)(10 * baseHudScaleFloat));
 		tx = (int)(25 * baseHudScaleFloat);
@@ -2425,7 +2427,7 @@ static void K_drawBackupItem(void)
 
 		V_DrawFixedPatch(
 			fx<<FRACBITS, (fy<<FRACBITS),
-			baseHudScaleFixed, transflag|V_SLIDEIN|fflags,
+			baseHudScaleFixed, backupitemflags,
 			localpatch[1], (localcolor[1] ? R_GetTranslationColormap(colormode[1], localcolor[1], GTC_CACHE) : NULL)
 		);
 
@@ -2433,17 +2435,17 @@ static void K_drawBackupItem(void)
 			V_DrawStringScaled(
 				(fx+tx)<<FRACBITS, (fy+ty)<<FRACBITS,
 				baseHudScaleFixed, baseHudScaleFixed, baseHudScaleFixed,
-				transflag|V_SLIDEIN|fflags, NULL, TINY_FONT, va("x%d", stplyr->backupitemamount)
+				backupitemflags, NULL, TINY_FONT, va("x%d", stplyr->backupitemamount)
 			);
 		} else {
-			V_DrawString(fx+tx, fy+ty, V_HUDTRANS|V_SLIDEIN|fflags, va("x%d", stplyr->backupitemamount));
+			V_DrawString(fx+tx, fy+ty, backupitemflags, va("x%d", stplyr->backupitemamount));
 		}
 	}
 	else
 	{
 		V_DrawFixedPatch(
 			fx<<FRACBITS, (fy<<FRACBITS),
-			baseHudScaleFixed, transflag|V_SLIDEIN|fflags,
+			baseHudScaleFixed, backupitemflags,
 			localpatch[1], (localcolor[1] ? R_GetTranslationColormap(colormode[1], localcolor[1], GTC_CACHE) : NULL)
 		);
 	}
